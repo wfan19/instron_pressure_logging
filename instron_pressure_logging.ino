d0fba358d1b8e04d1a6a4c67f6c0f19998fc97e2
Bill Fan <fanbenlong@gmail.com>
2023-07-12 12:30:12 -0400

    Initial commit: stream of sensor voltage readings

---
+++ instron_pressure_logging.ino
@@ -0,0 +1,44 @@
+int instron_signal_pin = 7;
+int pressure_sensor_pin = A0;
+unsigned long start_time = 0;
+
+bool last_logging = 0;
+
+void setup() {
+  pinMode(instron_signal_pin, INPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  bool new_logging = digitalRead(instron_signal_pin);
+  if ((last_logging == 0) && (new_logging == 1)) {
+    start_time = millis();
+  }
+  //Serial.println("Logging: " + String(new_logging));
+
+  // Read the pressure, and print it if we are logging
+//  float pressure_volts = analogRead(pressure_sensor_pin);
+//  float pressure = v_to_pressure(pressure_volts);
+//  if (new_logging) {
+//    unsigned long t = millis() - start_time;
+//    Serial.println(String(t) + "," + String(pressure));
+//  }
+//
+//  last_logging = new_logging;
+
+  float pressure_volts = analogRead(pressure_sensor_pin);
+  Serial.println("Analog read: " + String(pressure_volts));
+}
+
+float v_to_pressure(float voltage) {
+  // Inversion of the pressure-to-voltage formula found in the datasheet:
+  // See pg. 11 of https://media.digikey.com/pdf/Data%20Sheets/Honeywell%20PDFs/ssc_series_DS.pdf
+  // Constants P_MAX and P_MIN are for SSCDRRN030PGAB5:
+  
+  float P_MAX = 30; // psi
+  float P_MIN = 0;  // psi
+  float V_SUPPLY = 5;
+
+  float pressure = (P_MAX - P_MIN)/(0.8 * V_SUPPLY) * (voltage - 0.1 * V_SUPPLY) + P_MIN;
+  return pressure;
+}
